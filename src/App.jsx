d4fc34a6dae240ea06c78784faf645ac49cf4733
David Naymon <naymondavid@gmail.com>
2024-09-17 11:06:54 -0400

    sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SideBar from './components/SideBar';
+import SignUp from './components/Signup';
+import LogIn from './components/Login';
+import LandingPage from './components/Landing';
+import AboutPage from './components/About';
+import AboutPageDash from './components/AboutDashboard';
+import Dashbaord from './components/Dashboard';
+import FavoritesPage from './components/Favorites';
 function App() {
  
 
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/SideBar" element={<SideBar />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/dashboard" element={<Dashbaord />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/about-dashboard" element={<AboutPageDash />} />
       </Routes>
     </BrowserRouter>
   );
