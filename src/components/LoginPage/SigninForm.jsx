a10d37867e306639f8dd2da19ebda6cd9fb35165
Brian Nguyen <nguyenbrian2323@gmail.com>
2024-09-18 08:00:43 -0700

    fixed ui element

--- src/components/LoginPage/SigninForm.jsx
+++ src/components/LoginPage/SigninForm.jsx
@@ -53,7 +53,7 @@ const SigninForm = () => {
                   <br></br>
                   to
                   <br></br>
-                  Jovial Jobial!
+                  Jobsmith!
                 </h1>
                 <div className='mt-11 flex flex-col items-center'>
                   <button className='w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline'>
